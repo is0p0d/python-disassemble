c2c15dd3f52bbd90c56829a16584de4128d4ade3
is0p0d <jpmoroney715@outlook.com>
2023-10-25 14:57:57 -0500

    argparsing finished

--- disassemble.py
+++ disassemble.py
@@ -8,16 +8,14 @@ if __name__ == "__main__":
     print('* disassemble.py                       *')
     print('****************************************')
 
-    numArgs = len(sys.argv)
-    print('numArgs: ', numArgs, " ",sys.argv)
-    if numArgs == 2:
-        fileName = sys.argv[1] # Filename if defined on commandline
-    elif numArgs < 2:
-	    fileName = input('(disassemble.py) Please specify a file: ')
-    elif numArgs > 2:
-        sys.exit('(disassemble.py) EXIT: too many arguments\n')
-    else:
-        sys.exit('(disassemble.py) EXIT: undefined behavior\n')
+    parser = argparse.ArgumentParser(
+                        prog='disassemble.py',
+                        description='A tool for inspecting the .text section of executables',
+                        epilog='jpm 2023')
+    parser.add_argument('filename',
+                        help='the file or path to file that you wish to inspect')
+    args = parser.parse_args()
+    fileName = args.filename
 
     try:
         file = open(fileName, "rb")
@@ -28,5 +26,7 @@ if __name__ == "__main__":
     except None:
         print('(disassemble.py) EXCEPTION: Null value encountered')
     else:
-        print('(disassemble.py) RUN:',filename,' opened, disassembling...')
+        print(f'(disassemble.py) RUN: {fileName} opened, disassembling...')
+        data = file.read()
+        
         
